0ed1462805b3b9bcfe2db351c06e0afcc318ba9f
IIVioletKingII <samdepoule@gmail.com>
2022-05-20 14:04:24 -0700

    Started on wifi and webserver stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,62 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #define ss 5
 #define rst 14
 #define dio0 2
 
+#define SSID "Base Station"
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println("LoRa Sender");
-  LoRa.setPins(ss, rst, dio0);
-  if(!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
-    digitalWrite(26, HIGH);
-    while (1);
-  }else{
-    Serial.println("yoot");
-  }
+	Serial.begin(115200);
+	while (!Serial);
+	Serial.println("LoRa Sender");
+	LoRa.setPins(ss, rst, dio0);
+	if(!LoRa.begin(433E6)) {
+		Serial.println("Starting LoRa failed!");
+		digitalWrite(26, HIGH);
+		while (1);
+	}else{
+		Serial.println("yoot");
+	}
+	WiFi.mode(WIFI_AP);
+	WiFi.softAP(SSID);
 }
 
 void loop() {
 
-  if(Serial.available() > 1){
-      LoRa.beginPacket();
-      LoRa.print("armed");
-      LoRa.endPacket();
-      Serial.println("Armed");
-      
-      delay(100);
-  }
+	if(Serial.available() > 1){
+		LoRa.beginPacket();
+		LoRa.print("armed");
+		LoRa.endPacket();
+		Serial.println("Armed");
+		
+		delay(100);
+	}
 
-  // send packet
+	// send packet
 
- int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
+	int packetSize = LoRa.parsePacket();
+	if (packetSize) {
+	// received a packet
+	Serial.print("Received packet '");
 
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
+	// read packet
+	while (LoRa.available()) {
+		Serial.print((char)LoRa.read());
+	}
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
+	// print RSSI of packet
+	Serial.print("' with RSSI ");
+	Serial.println(LoRa.packetRssi());
+	}
   
 }
